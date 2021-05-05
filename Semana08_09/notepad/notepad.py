be17efe4686eaf95f3ee3106ae82e838feca86c2
pablobarbosantos <63117998+pablobarbosantos@users.noreply.github.com>
2021-05-04 21:27:09 -0300

    Add files via upload

---
+++ Semana08_09/notepad/notepad.py
@@ -0,0 +1,131 @@
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtPrintSupport import *
+
+import os
+import sys
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        layout = QVBoxLayout()
+        self.editor = QPlainTextEdit()
+
+        fixedfont = QFontDatabase.systemFont(QFontDatabase.FixedFont)
+        fixedfont.setPointSize(12)
+        self.editor.setFont(fixedfont)
+        self.path = None
+        layout.addWidget(self.editor)
+
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+
+        self.status = QStatusBar()
+        self.setStatusBar(self.status)
+
+        file_toolbar = QToolBar("File")
+        file_toolbar.setIconSize(QSize(14, 14))
+        self.addToolBar(file_toolbar)
+        file_menu = self.menuBar().addMenu("&File")
+
+        open_file_action = QAction(QIcon(os.path.join(
+            'images', 'blue-folder-open-document.png')), "Open file...", self)
+        open_file_action.setStatusTip("Open file")
+        open_file_action.triggered.connect(self.file_open)
+        file_menu.addAction(open_file_action)
+        file_toolbar.addAction(open_file_action)
+
+        save_file_action = QAction(
+            QIcon(os.path.join('images', 'disk.png')), "Save", self)
+        save_file_action.setStatusTip("Save current page")
+        save_file_action.triggered.connect(self.file_save)
+        file_menu.addAction(save_file_action)
+        file_toolbar.addAction(save_file_action)
+
+        saveas_file_action = QAction(
+            QIcon(os.path.join('images', 'disk--pencil.png')), "Save As...", self)
+        saveas_file_action.setStatusTip("Save current page to specified file")
+        saveas_file_action.triggered.connect(self.file_saveas)
+        file_menu.addAction(saveas_file_action)
+        file_toolbar.addAction(saveas_file_action)
+
+        self.update_title()
+        self.show()
+
+    def dialog_critical(self, s):
+        dlg = QMessageBox(self)
+        dlg.setText(s)
+        dlg.setIcon(QMessageBox.Critical)
+        dlg.show()
+
+    def file_open(self):
+        path, _ = QFileDialog.getOpenFileName(
+            self, "Open file", "", "Text documents (*.txt);All files (*.*)")
+
+        if path:
+            try:
+                with open(path, 'rU') as f:
+                    text = f.read()
+
+            except Exception as e:
+                self.dialog_critical(str(e))
+
+            else:
+                self.path = path
+                self.editor.setPlainText(text)
+                self.update_title()
+
+    def file_save(self):
+        if self.path is None:
+            return self.file_saveas()
+
+        self._save_to_path(self.path)
+
+    def file_saveas(self):
+        path, _ = QFileDialog.getSaveFileName(
+            self, "Save file", "", "Text documents (*.txt);All files (*.*)")
+
+        if not path:
+            return
+
+        self._save_to_path(path)
+
+    def _save_to_path(self, path):
+        text = self.editor.toPlainText()
+        try:
+            with open(path, 'w') as f:
+                f.write(text)
+
+        except Exception as e:
+            self.dialog_critical(str(e))
+
+        else:
+            self.path = path
+            self.update_title()
+
+    def file_print(self):
+        dlg = QPrintDialog()
+        if dlg.exec_():
+            self.editor.print_(dlg.printer())
+
+    def update_title(self):
+        self.setWindowTitle(
+            "%s - No2Pads" % (os.path.basename(self.path) if self.path else "Untitled"))
+
+    def edit_toggle_wrap(self):
+        self.editor.setLineWrapMode(
+            1 if self.editor.lineWrapMode() == 0 else 0)
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    app.setApplicationName("No2Pads")
+
+    window = MainWindow()
+    app.exec_()
